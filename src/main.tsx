c91938e006ecaf6b3397641d3753578f08b3630d
ha-ccoon <codeminhasohn@gmail.com>
2025-03-23 17:51:40 +0900

    chore: npm add @aws-amplify/ui-react

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+
 import App from './App.tsx';
+
+import '@aws-amplify/ui-react/styles.css';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </StrictMode>,
 );
