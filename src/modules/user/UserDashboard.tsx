c0ae7903fc5bb8da6194547a0b039613cd3d33be
ha-ccoon <codeminhasohn@gmail.com>
2025-03-23 12:05:19 +0900

    chore: add basic table

--- src/modules/user/UserDashboard.tsx
+++ src/modules/user/UserDashboard.tsx
@@ -1,6 +1,13 @@
+import { DataTable } from '@/components/common/DataTable';
+import { tableMock } from '@/constants/mock';
+
+import { columns } from './columns';
+
 export function UserDashboard() {
   return (
     <>
+      <DataTable columns={columns} data={tableMock} />
+
       <div className="flex flex-1 flex-col gap-4 p-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
           <div className="aspect-video rounded-xl bg-muted/50" />
