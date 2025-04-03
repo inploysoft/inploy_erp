9cd065b6c446fdc1910b27ff8bebff5b65853843
ha-ccoon <codeminhasohn@gmail.com>
2025-04-03 22:56:44 +0900

    chore: create fetchPurchasedModules lamda function

---
+++ amplify/functions/fetchPurchasedModules/handler.ts
@@ -0,0 +1,76 @@
+import { Amplify } from 'aws-amplify';
+
+import { env } from '$amplify/env/fetchPurchasedModules';
+
+import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
+import { generateClient } from 'aws-amplify/api';
+
+import { type Schema } from '../../data/resource';
+
+const { resourceConfig, libraryOptions } =
+  await getAmplifyDataClientConfig(env);
+
+Amplify.configure(resourceConfig, libraryOptions);
+
+const client = generateClient<Schema>();
+
+export const handler: Schema['fetchPurchasedModules']['functionHandler'] =
+  async (event) => {
+    const { sub } = event.arguments;
+
+    if (!sub) {
+      return null;
+    }
+
+    const { data: companyUser, errors: companyUserErrors } =
+      await client.models.CompanyUser.list({
+        authMode: 'userPool',
+        filter: {
+          sub: { eq: sub },
+        },
+      });
+
+    if (companyUserErrors || !companyUser[0].companyId) {
+      console.log('FetchUserError: ', companyUserErrors);
+      return null;
+    }
+
+    const { data: company, errors: companyErrors } =
+      await client.models.Company.get(
+        {
+          id: companyUser[0].companyId,
+        },
+        {
+          authMode: 'userPool',
+        },
+      );
+
+    if (companyErrors || !company?.purchasedModuleId) {
+      console.log('FetchUserError: ', companyErrors);
+      return null;
+    }
+
+    const { data: purchasedModule, errors: purchasedModuleErrors } =
+      await client.models.PurchasedModule.list({
+        authMode: 'userPool',
+        filter: {
+          companyId: { eq: companyUser[0].companyId! },
+        },
+        selectionSet: [
+          'id',
+          'company.name',
+          'status',
+          'module.*',
+          'moduleInstanceId.status',
+          'moduleInstanceId.entityFieldSchemaIds.*',
+          'moduleInstanceId.memberId.*',
+        ],
+      });
+
+    if (purchasedModuleErrors) {
+      console.log('FetchUserError: ', purchasedModuleErrors);
+      return null;
+    }
+
+    return purchasedModule;
+  };
