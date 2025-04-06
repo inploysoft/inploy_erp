28288ddea3f927a022705a95df64cb6ecb82c1e3
ha-ccoon <codeminhasohn@gmail.com>
2025-04-06 18:16:18 +0900

    chore: change the table relationship

--- src/types/responseTypes.ts
+++ src/types/responseTypes.ts
@@ -9,7 +9,8 @@ export const selectionSet = [
   'module.*',
   'moduleInstanceId.status',
   'moduleInstanceId.entityFieldSchemaIds.*',
-  'moduleInstanceId.memberId.*',
+  'moduleInstanceId.memberIds.*',
+  'moduleInstanceId.membershipIds.*',
 ] as const;
 
 export type FetchPurchasedModule = SelectionSet<
