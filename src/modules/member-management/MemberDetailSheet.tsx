4b6546930be632529911cfc2e6bf09ef45ed47ea
ha-ccoon <codeminhasohn@gmail.com>
2025-04-13 00:14:06 +0900

    chore: create MembershipCard component

--- src/modules/member-management/MemberDetailSheet.tsx
+++ src/modules/member-management/MemberDetailSheet.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import {
   Sheet,
   SheetContent,
@@ -5,6 +7,12 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet/sheet';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { H4 } from '@/theme/Typography';
+import {
+  EmptyMembershipMessage,
+  RenderMembershipCard,
+} from './components/MembershipCard';
 import { MemberTableData } from './types/views';
 
 interface MemberDetailSheetProps {
@@ -20,14 +28,57 @@ export function MemberDetailSheet({
 }: MemberDetailSheetProps) {
   return (
     <Sheet open={open} onOpenChange={setOpen}>
-      <SheetContent className="w-full max-w-none sm:w-[50vw]">
+      <SheetContent className="w-full max-w-none p-2 sm:w-[50vw]">
         <SheetHeader>
-          <SheetTitle>{member.name}</SheetTitle>
+          <SheetTitle className="text-2xl font-bold">{member.name}</SheetTitle>
 
           <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            {member.birthDate} | {member.gender} | {member.phone}
           </SheetDescription>
+
+          <Card className="min-h-[25rem] w-full">
+            <CardHeader>
+              <CardTitle>
+                <H4>이용권</H4>
+              </CardTitle>
+            </CardHeader>
+
+            <CardContent>
+              <Tabs defaultValue="all">
+                <TabsList>
+                  <TabsTrigger value="all">전체</TabsTrigger>
+                  <TabsTrigger value="valid">사용 중</TabsTrigger>
+                  <TabsTrigger value="expired">만료</TabsTrigger>
+                </TabsList>
+
+                <ScrollArea className="h-[20rem] pt-3">
+                  <TabsContent value="all">
+                    {member.memberships.map(RenderMembershipCard)}
+                  </TabsContent>
+
+                  <TabsContent value="valid">
+                    {member.memberships.some((m) => m.status === 'valid') ? (
+                      member.memberships
+                        .filter((m) => m.status === 'valid')
+                        .map(RenderMembershipCard)
+                    ) : (
+                      <EmptyMembershipMessage message="사용 중인 이용권이 없어요" />
+                    )}
+                  </TabsContent>
+
+                  <TabsContent value="expired">
+                    {member.memberships.some((m) => m.status === 'expired') ? (
+                      member.memberships
+                        .filter((m) => m.status === 'expired')
+                        .map(RenderMembershipCard)
+                    ) : (
+                      <EmptyMembershipMessage message="만료된 이용권이 없어요" />
+                    )}
+                  </TabsContent>
+                </ScrollArea>
+              </Tabs>
+            </CardContent>
+          </Card>
         </SheetHeader>
       </SheetContent>
     </Sheet>
--- src/modules/member-management/models/membershipRegistration.ts
+++ src/modules/member-management/models/membershipRegistration.ts
@@ -1,12 +1,14 @@
 import { CustomField, Nullable } from '@/shared/types';
 
+export type MembershipRegistrationStatus = 'valid' | 'expired';
+
 export interface MembershipRegistration {
   moduleInstanceId: Nullable<string>;
   memberId: Nullable<string>;
   membershipId: Nullable<string>;
   id: string;
   //
-  status: Nullable<string>;
+  status: Nullable<MembershipRegistrationStatus>;
   usedSessionCount: Nullable<number>;
   registeredAt: Nullable<string>;
   expiredAt: Nullable<string>;
