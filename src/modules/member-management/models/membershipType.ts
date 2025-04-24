ef3c3e01331a865f0f27db9fd26fc647a9bad770
ha-ccoon <codeminhasohn@gmail.com>
2025-04-24 23:56:48 +0900

    chore: define membership models

---
+++ src/modules/member-management/models/membershipType.ts
@@ -0,0 +1,13 @@
+import { CustomField, Nullable } from '@/shared/types/types';
+
+export interface MembershipType {
+  moduleInstanceId: Nullable<string>;
+  id: string;
+  //
+  displayName: Nullable<string>;
+  isActive: boolean;
+  description: Nullable<string>;
+  customFields: Nullable<CustomField>;
+  createdAt: string;
+  updatedAt: string;
+}
