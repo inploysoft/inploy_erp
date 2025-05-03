38d581da53b27bccbb9a911adecd6c51baa702bf
ha-ccoon <codeminhasohn@gmail.com>
2025-05-03 14:50:27 +0900

    chore: npm uninstall @aws-amplify/ui-react

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import outputs from '../amplify_outputs.json';
 import App from './App';
 
-import '@aws-amplify/ui-react/styles.css';
-
 import './theme/index.css';
 
 // https://docs.amplify.aws/react/build-a-backend/data/connect-to-API/#configure-the-amplify-library
@@ -21,8 +19,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      {/* <InployAuthenticator>
-      </InployAuthenticator> */}
     </QueryClientProvider>
   </StrictMode>,
 );
