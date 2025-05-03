530b431bdeb01edee8807434ab04079443573847
ha-ccoon <codeminhasohn@gmail.com>
2025-05-03 22:45:17 +0900

    chore: add create event function with selectable
    - https://jquense.github.io/react-big-calendar/examples/index.html?path=/docs/examples--example-2

--- src/modules/scheduler/CalendarPage.tsx
+++ src/modules/scheduler/CalendarPage.tsx
@@ -1,22 +1,66 @@
-import { DateTime } from 'luxon';
+import { useCallback, useMemo, useState } from 'react';
 
-import { Calendar, luxonLocalizer } from 'react-big-calendar';
+import { DateTime } from 'luxon';
+import { Calendar, luxonLocalizer, Views } from 'react-big-calendar';
+
 import { events, messagesKo, resources } from './lib/constant';
 
 const localizer = luxonLocalizer(DateTime);
 
 export function CalendarPage() {
+  const [myEvents, setEvents] = useState(events);
+
+  const handleSelectSlot = useCallback(
+    ({ start, end }) => {
+      const title = window.prompt('New Event name');
+
+      if (title) {
+        setEvents((prev) => [
+          ...prev,
+          {
+            id: 224,
+            resourceId: 1,
+            title: title,
+            start: start,
+            end: end,
+          },
+        ]);
+      }
+    },
+    [setEvents],
+  );
+
+  const handleSelectEvent = useCallback(
+    (event) => window.alert(event.title),
+    [],
+  );
+
+  const { scrollToTime, messages } = useMemo(
+    () => ({
+      scrollToTime: new Date(1970, 1, 1, 6),
+      messages: messagesKo,
+    }),
+    [],
+  );
+
   return (
     <>
       <Calendar
         culture="ko"
+        defaultView={Views.WEEK}
         endAccessor="end"
-        events={events}
+        events={myEvents}
+        //
+        onSelectEvent={handleSelectEvent}
+        onSelectSlot={handleSelectSlot}
+        selectable
+        scrollToTime={scrollToTime}
+        //
         resources={resources}
         resourceIdAccessor="trainerId"
         resourceTitleAccessor="trainerName"
         localizer={localizer}
-        messages={messagesKo}
+        messages={messages}
         startAccessor="start"
       />
     </>
