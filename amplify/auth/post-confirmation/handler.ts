692f1b4daa327ad1dbcc4e1cd909636d86bb0325
ha-ccoon <codeminhasohn@gmail.com>
2025-05-03 14:20:07 +0900

    chore: delete hardcode

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -1,12 +1,10 @@
-import { Amplify } from 'aws-amplify';
-
 import { env } from '$amplify/env/post-confirmation';
-
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
 import {
   AdminAddUserToGroupCommand,
   CognitoIdentityProviderClient,
 } from '@aws-sdk/client-cognito-identity-provider';
+import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/data';
 import type { PostConfirmationTriggerHandler } from 'aws-lambda';
 
@@ -14,17 +12,19 @@ import { type Schema } from '../../data/resource';
 
 import dayjs from 'dayjs';
 
+const authClient = new CognitoIdentityProviderClient();
+
 // https://docs.amplify.aws/react/build-a-backend/data/customize-authz/grant-lambda-function-access-to-api/
 const { resourceConfig, libraryOptions } =
   await getAmplifyDataClientConfig(env);
 
 Amplify.configure(resourceConfig, libraryOptions);
 
-const authClient = new CognitoIdentityProviderClient();
-
 const dataClient = generateClient<Schema>();
 
 export const handler: PostConfirmationTriggerHandler = async (event) => {
+  console.log('here', event.request.userAttributes);
+
   const isAdmin = event.request.userAttributes['custom:is_admin'];
 
   const groupName = isAdmin ? env.GROUP_ADMINS : env.GROUP_USERS;
@@ -36,23 +36,17 @@ export const handler: PostConfirmationTriggerHandler = async (event) => {
   });
 
   try {
-    const response = await authClient.send(command);
+    await authClient.send(command);
 
-    console.log(`User ${event.userName} added to ${groupName} group`);
-    console.log('processed', response.$metadata.requestId);
-
-    // TODO: 20250326 하드코딩 수정 필요
-    const createUser = await dataClient.models.CompanyMember.create({
+    await dataClient.models.CompanyMember.create({
       sub: event.request.userAttributes['sub'],
-      companyId: 'd9244152-3bbd-4a02-938f-86e561ec9d8b',
+      companyId: event.request.userAttributes['company_id'],
       email: event.request.userAttributes['email'],
       isAdmin: isAdmin === 'true',
-      name: '',
-      phone: '',
+      name: event.request.userAttributes['name'],
+      phone: event.request.userAttributes['phone_number'],
       joinedAt: dayjs().format('YYYY-MM-DD'),
     });
-
-    console.log('createUser', createUser);
   } catch (error) {
     console.error(`Error adding user to ${groupName} group:`, error);
   }
