ad5d903075281eb902482bca64b92a4bba1fbc4f
ha-ccoon <codeminhasohn@gmail.com>
2025-05-03 18:31:52 +0900

    chore: implement basic calendar

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import outputs from '../amplify_outputs.json';
 import App from './App';
 
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 import './theme/index.css';
 
 // https://docs.amplify.aws/react/build-a-backend/data/connect-to-API/#configure-the-amplify-library
--- src/modules/scheduler/CalendarPage.tsx
+++ src/modules/scheduler/CalendarPage.tsx
@@ -1,7 +1,15 @@
+import { DateTime } from 'luxon';
+
+import { Calendar, luxonLocalizer } from 'react-big-calendar';
+
+const localizer = luxonLocalizer(DateTime, { firstDayOfWeek: 7 });
+
 export function CalendarPage() {
   return (
     <>
       <h1>CalendarPage</h1>
+
+      <Calendar localizer={localizer} startAccessor="start" endAccessor="end" />
     </>
   );
 }
